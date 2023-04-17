be06bfda4760da7bdc9293665020d95d42df4f7e
Maksim Aniska <aniskamaksim@gmail.com>
2023-04-17 18:39:54 +0300

    feat: add redux and html section, change some layout in about section, create a first version of portfolio

--- portfolio/src/pages/JSTasks.tsx
+++ portfolio/src/pages/JSTasks.tsx
@@ -8,17 +8,19 @@ export const JSTasks = () => {
     return (
         <div className={mainJsTask}>
             <div className={linkList}>
+               <div style={{color: "white", marginLeft: "2.2em", marginTop: "1.5em", fontSize: "large"}}>Hundreds of hours spent learning Javascript cannot be useless. <br/>
+                    By clicking on the links you can find an example of my code and how I solved various tasks.</div>
             <ol className={xtext}>
-                <li><a href="https://github.com/aniskamaksim/typical-arrays-problems">typical-arrays-problems</a></li>
-                <li><a href="https://github.com/aniskamaksim/basic-js">basic-js</a></li>
-                <li><a href="https://github.com/aniskamaksim/brackets">brackets</a></li>
-                <li><a href="https://github.com/aniskamaksim/towel-sort">towel-sort</a></li>
-                <li><a href="https://github.com/aniskamaksim/morse-decoder">morse-decoder</a></li>
-                <li><a href="https://github.com/aniskamaksim/reverse-int">reverse-int</a></li>
-                <li><a href="https://github.com/aniskamaksim/human-readable-number">human-readable-number</a></li>
+                <li><a href="https://github.com/aniskamaksim/typical-arrays-problems" target={"_blank"} rel={"noreferrer"}>typical-arrays-problems</a></li>
+                <li><a href="https://github.com/aniskamaksim/basic-js" target={"_blank"} rel={"noreferrer"}>basic-js</a></li>
+                <li><a href="https://github.com/aniskamaksim/brackets" target={"_blank"} rel={"noreferrer"}>brackets</a></li>
+                <li><a href="https://github.com/aniskamaksim/towel-sort" target={"_blank"} rel={"noreferrer"}>towel-sort</a></li>
+                <li><a href="https://github.com/aniskamaksim/morse-decoder" target={"_blank"} rel={"noreferrer"}>morse-decoder</a></li>
+                <li><a href="https://github.com/aniskamaksim/reverse-int" target={"_blank"} rel={"noreferrer"}>reverse-int</a></li>
+                <li><a href="https://github.com/aniskamaksim/human-readable-number" target={"_blank"} rel={"noreferrer"}>human-readable-number</a></li>
             </ol>
         </div>
-            <h2 style={{color: "#f2f2f2", marginRight: "1em"}}>More my code practice on codwars: <a href="https://www.codewars.com/users/aniskaMaksim" target={"_blank"}>@aniskaMaksim</a></h2>
+            <h3 style={{color: "white", marginRight: "1em"}}>More my code practice on codwars: <a href="https://www.codewars.com/users/aniskaMaksim" target={"_blank"} rel={"noreferrer"}>@aniskaMaksim</a></h3>
         </div>
     );
 };
--- portfolio/src/pages/ReactRedux.tsx
+++ portfolio/src/pages/ReactRedux.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import s from "./Pages.module.css";
+import simple_counter from "../Assets/simple_counter.png";
 
 export const ReactRedux = () => {
+    const main = s.react_projects_main;
+    const list = s.react_projects_list;
+    const white = s.white_span;
+    const titleDescription = s.title_description;
+    const titleTitle = s.title_title;
+    const descrDescr = s.description_description;
+    const liDescr = s.li_description;
+    const preview = s.react_projects_preview;
     return (
-        <div>
-            React-Redux page
+        <div className={main}>
+            <div className={list}>
+                <div className={titleDescription}>
+                    <div className={titleTitle}>
+                        <h2 className={white}>
+                            <a href="https://aniskamaksim.github.io/counter/" target={"_blank"} rel={"noreferrer"}>Simple counter
+                                with settings</a>
+                        </h2>
+                        <div className={descrDescr}>
+                    <span className={white}>
+                       react-redux - Redux - TypeScript <br/>
+                        Simple counter with settings of min and max value<br/>
+                        Implemented in this app:
+                            <ul>
+                            <li className={liDescr}>store;</li>
+                            <li className={liDescr}>useState, useSelector, useDispatch hooks;</li>
+                            <li className={liDescr}>material UI;</li>
+                                </ul>
+                    </span><br/>
+                            <a href="https://aniskamaksim.github.io/counter/"
+                               target={"_blank"} rel={"noreferrer"}> https://aniskamaksim.github.io/messages_counter/</a>
+                        </div>
+                    </div>
+                </div>
+                <div className={preview}>
+                    <a href="https://aniskamaksim.github.io/counter/" target={"_blank"} rel={"noreferrer"}>
+                        <img src={simple_counter}
+                             alt="message counter"
+                             width={"200px"}
+                             height={"200px"}
+                        />
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
